f1e2a85b4a546e86034ebd80e4f822f759038a50
Jason Hargrove <rijaha@gmail.com>
2024-01-25 17:00:33 -0600

    Added MongoDB

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
-const express = require('express')
-const app = express()
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import { error } from 'console'
 
+dotenv.config()
+
+const app = express()
 const PORT = process.env.PORT || 5000
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB connected...'))
+  .catch((err) => console.log(err))
+
+// Middleware
+app.use(express.json()) // This allows us to acccept JSON in requests.
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
